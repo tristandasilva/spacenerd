a8b08cf4ff847dc8344cccda54f495f8ffdeec72
Tristan <tristandasilva@Tristans-MacBook-Air.local>
2021-04-10 20:31:16 -0400

    Initial react-bootstrap template setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
-   return <h1>Hi</h1>
+   return (
+      <>
+         <Header />
+         <main>
+            <Container>ALL CONTENT GOES HERE!</Container>
+         </main>
+         <Footer />
+      </>
+   )
 }
 
 export default App
