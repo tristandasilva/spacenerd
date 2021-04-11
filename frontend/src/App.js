d2f8a0d0f5708150d141f548233412ff66b2308f
Tristan <tristandasilva@Tristans-MacBook-Air.local>
2021-04-10 21:18:57 -0400

    Loading products onto homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
    return (
       <>
          <Header />
          <main>
-            <Container>ALL CONTENT GOES HERE!</Container>
+            <Container>
+               <HomeScreen />
+            </Container>
          </main>
          <Footer />
       </>
