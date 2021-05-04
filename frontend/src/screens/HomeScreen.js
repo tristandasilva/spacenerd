d5bacd218124212ab6dd232fec3e5af12a881ff3
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-04 13:59:59 -0400

    Adding rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
          <h1 className='mt-4'>Latest Products</h1>
          <Row>
             {products.map(product => (
-               <Col sm={12} med={6} lg={4} xl={3}>
+               <Col key={product._id} sm={12} med={6} lg={4} xl={3}>
                   <Product product={product} />
                </Col>
             ))}
