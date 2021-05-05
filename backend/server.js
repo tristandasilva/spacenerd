fa65651e32e6c7af75495001fcdd2bc4902f1400
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-05 13:46:05 -0400

    Adding nodemon and concurrently, environment variables, and ES Modules'

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -15,4 +19,9 @@ app.get('/api/products/:id', (req, res) => {
    res.json(product)
 })
 
-app.listen(5000, console.log('Server running'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+   PORT,
+   console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
