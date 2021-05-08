59688aecdc6df045831f7777e39dce3b5f0a0133
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-08 16:03:34 -0400

    Product details reducer and action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,6 +6,12 @@ import {
    PRODUCT_LIST_FAIL,
 } from '../constants/productConstants'
 
+import {
+   PRODUCT_DETAILS_REQUEST,
+   PRODUCT_DETAILS_SUCCESS,
+   PRODUCT_DETAILS_FAIL,
+} from '../constants/productConstants'
+
 export const listProducts = () => async dispatch => {
    try {
       dispatch({ type: PRODUCT_LIST_REQUEST })
@@ -19,6 +25,27 @@ export const listProducts = () => async dispatch => {
    } catch (err) {
       dispatch({
          type: PRODUCT_LIST_FAIL,
+         payload:
+            err.response && err.response.data.message
+               ? err.response.data.message
+               : err.message,
+      })
+   }
+}
+
+export const listProductDetails = id => async dispatch => {
+   try {
+      dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      dispatch({
+         type: PRODUCT_DETAILS_SUCCESS,
+         payload: data,
+      })
+   } catch (err) {
+      dispatch({
+         type: PRODUCT_DETAILS_FAIL,
          payload:
             err.response && err.response.data.message
                ? err.response.data.message
