25301e66de9515b0203d7c905348bfefb024d08c
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-08 14:03:41 -0400

    Fetching data from database and adding error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -13,14 +14,10 @@ app.get('/', (req, res) => {
    res.send('You rock')
 })
 
-app.get('/api/products', (req, res) => {
-   res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-   const product = products.find(product => product._id === req.params.id)
-   res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
