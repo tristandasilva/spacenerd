e1518f3b989c79f36a9d7be5d1f8350ea85c9164
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-09 12:31:04 -0400

    Add to cart functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Message from '../components/Message'
 import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = ({ match, history }) => {
-   const [qty, setQty] = useState(0)
+   const [qty, setQty] = useState(1)
 
    const dispatch = useDispatch()
 
