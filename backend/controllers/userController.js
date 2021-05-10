5ed9a5013fe93b8b15873d1fc000d3d39fa15600
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-10 19:11:56 -0400

    User register reducer, action and screen + User update profile & profile screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,6 +43,35 @@ export const getUserProfile = asyncHandler(async (req, res) => {
    }
 })
 
+// @desc  Update user profile
+// @route PUT /api/users/profile
+// @access Private
+export const updateUserProfile = asyncHandler(async (req, res) => {
+   const user = await User.findById(req.user._id)
+
+   if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+
+      if (req.body.password) {
+         user.password = req.body.password
+      }
+
+      const updatedUser = await user.save()
+
+      res.json({
+         _id: updatedUser._id,
+         name: updatedUser.name,
+         email: updatedUser.email,
+         isAdmin: updatedUser.isAdmin,
+         token: generateToken(user._id),
+      })
+   } else {
+      res.status(401)
+      throw new Error('User not found')
+   }
+})
+
 // @desc  Register a new user
 // @route POST /api/users
 // @access Public
