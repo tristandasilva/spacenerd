e2d2d1f21bf9b0938b7b8ccef7d14c76d0edb329
Tristan Dasilva <tristan.dasilva98@gmail.com>
2021-05-14 14:31:19 -0400

    Fixing order details bug when logging out and logging back in from a different user

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -13,6 +13,7 @@ import {
    ORDER_LIST_USER_SUCCESS,
    ORDER_LIST_USER_FAIL,
    ORDER_LIST_USER_RESET,
+   ORDER_CREATE_RESET,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -32,6 +33,8 @@ export const orderCreateReducer = (state = {}, action) => {
             loading: false,
             error: action.payload,
          }
+      case ORDER_CREATE_RESET:
+         return {}
       default:
          return state
    }
